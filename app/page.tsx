8a7fbe6f436ea1a70805149d211538c42bb9dc78
DavBN <162839191+DavBN@users.noreply.github.com>
2024-07-04 11:43:46 -0500

    Proceso actual

--- app/page.tsx
+++ app/page.tsx
@@ -1,4 +1,7 @@
+import BannerDiscount from "@/components/banner-discount";
+import BannerProduct from "@/components/banner-product";
 import CarouselTextBanner from "@/components/carousel-text-banner";
+import ChooseCategory from "@/components/choose-category";
 import FeatureProducts from "@/components/featured-products";
 import { Button } from "@/components/ui/button";
 import Image from "next/image";
@@ -8,6 +11,9 @@ export default function Home() {
     <main className="flex min-h-screen flex-col items-center justify-between p-24">
         <CarouselTextBanner/>
         <FeatureProducts />
+        <BannerDiscount/>
+        <ChooseCategory/>
+        <BannerProduct />
     </main>
   );
 }
